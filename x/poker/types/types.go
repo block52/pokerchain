fe51462f01d8566af244cfad7691341b2cc44117
tommycp3 <thomasmillerprivate@gmail.com>
2025-10-25 13:17:41 +1100

    fix: correct token denomination from 'uusdc' to 'usdc'

--- x/poker/types/types.go
+++ x/poker/types/types.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// TokenDenom is the denomination of the game token
-	TokenDenom = "uusdc"
+	TokenDenom = "usdc"
 
 	// GameCreationCost is the cost in tokens to create a new game
 	GameCreationCost = int64(1)
