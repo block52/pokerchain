52d005f8f7a02cb813d4e254463ec1776a5e56fb
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2025-11-21 05:05:07 +1000

    chore: add trailing slash to fix websockets

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -198,7 +198,7 @@ server {
 
     # PVM WebSocket endpoint on /ws path
     location /ws {
-        proxy_pass http://127.0.0.1:8545;
+        proxy_pass http://127.0.0.1:8545/;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -438,7 +438,7 @@ server {
 
     # PVM WebSocket endpoint on /ws path
     location /ws {
-        proxy_pass http://127.0.0.1:8545;
+        proxy_pass http://127.0.0.1:8545/;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
